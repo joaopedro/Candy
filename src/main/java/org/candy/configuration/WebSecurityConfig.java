acd4acdec9e6c0627e1773ebbcc6ac19a9da628f
João Pedro <pedro.joao@gmail.com>
2015-07-31 17:33:10 +0100

    Simple Authentication

---
+++ src/main/java/org/candy/configuration/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package org.candy.configuration;
+
+import org.candy.configuration.xauth.XAuthTokenConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityConfigurer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@EnableWebMvcSecurity
+@EnableWebSecurity(debug = false)
+@Configuration
+@Order
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests().antMatchers("/**/*.js","/**/*.css","/**/*.html").permitAll();
+
+        String[] restEndpointsToSecure = { "api","manage"};
+        for (String endpoint : restEndpointsToSecure) {
+            http.authorizeRequests().antMatchers("/" + endpoint + "/**").authenticated();
+        }
+
+
+        SecurityConfigurer<DefaultSecurityFilterChain, HttpSecurity> securityConfigurerAdapter = new XAuthTokenConfigurer(userDetailsServiceBean());
+        http.apply(securityConfigurerAdapter);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+        authManagerBuilder.userDetailsService(new CustomUserDetailsService());
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,16 +1,43 @@
 'use strict';
 
+var xAuthTokenHeaderName = 'x-auth-token';
+
 // Declare app level module which depends on views, and components
 angular.module('candyApp', [
     'ngRoute',
+    'ngCookies',
     'datatables' ,
+    'candyApp.services',
+    'candyApp.login',
     'candyApp.welcome',
     'candyApp.entity',
     'candyApp.manage',
     'candyApp.version'
 ])
-    .config(['$routeProvider', function ($routeProvider) {
+    .config(['$routeProvider','$httpProvider','$provide', function ($routeProvider, $httpProvider, $provide) {
         $routeProvider.otherwise({redirectTo: '/welcome'});
+
+        $provide.factory('responseInterceptor', function($rootScope, $q, $location) {
+            return {
+                // optional method
+                'responseError': function(rejection) {
+                    var status = rejection.status;
+                    var config = rejection.config;
+                    var method = config.method;
+                    var url = config.url;
+
+                    if (status == 403) {
+                        $location.path( "/login" );
+                    } else {
+                        $rootScope.error = method + " on " + url + " failed with status " + status;
+                    }
+
+                    return $q.reject(rejection);                }
+            };
+        });
+
+        $httpProvider.interceptors.push('responseInterceptor');
+
     }])
     .directive('footer', function () {
         return {
@@ -25,7 +52,7 @@ angular.module('candyApp', [
             replace: true,
             scope: {user: '='}, // This is one of the cool things :). Will be explained in post.
             templateUrl: "/header.html",
-            controller: ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
+            controller: ['$rootScope', '$scope', '$filter', '$http', function ($rootScope, $scope, $filter, $http) {
                 var request = {
                     method: 'GET',
                     url: 'manage/entity',
@@ -33,7 +60,48 @@ angular.module('candyApp', [
                 $http(request).success(function (response) {
                     $scope.entities = response._embedded.entity;
                 });
-
+                $scope.user = $rootScope.user;
+                $scope.logout = $rootScope.logout;
             }]
+        }
+    })
+    .run(function($rootScope, $http, $location, $cookieStore, LoginService) {
+        /* Reset error when a new view is loaded */
+        $rootScope.$on('$viewContentLoaded', function() {
+            delete $rootScope.error;
+        });
+
+        $rootScope.hasRole = function(role) {
+
+            if ($rootScope.user === undefined) {
+                return false;
+            }
+
+            if ($rootScope.user.roles[role] === undefined) {
+                return false;
+            }
+
+            return $rootScope.user.roles[role];
+        };
+
+
+
+        $rootScope.logout = function() {
+            debugger;
+            delete $rootScope.user;
+            delete $http.defaults.headers.common[xAuthTokenHeaderName];
+            $cookieStore.remove('user');
+            $location.path("/login");
+        };
+
+        /* Try getting valid user from cookie or go to login page */
+        var originalPath = $location.path();
+        $location.path("/login");
+        var user = $cookieStore.get('user');
+        if (user !== undefined) {
+            $rootScope.user = user;
+            $http.defaults.headers.common[xAuthTokenHeaderName] = user.token;
+
+            $location.path(originalPath);
         }
     });
